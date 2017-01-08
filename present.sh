8801dff160f2142d096a700b1cd305ab5d5cb1a3
autodesk <wouter.simons+autodesk@gmail.com>
2017-01-08 17:08:43 +0000

    added script that checks if i am home

--- present.sh
+++ present.sh
@@ -7,5 +7,11 @@ then
   echo 1
 else
 # cellphone is not present asuming i am not home turn of everything 
-  gpio write 2 1 & gpio write 7 1 & gpio write 8 1 & gpio write 15 1
+  gpio write 2 1
+  sleep 1
+  gpio write 7 1
+  sleep 1
+  gpio write 8 1
+  sleep
+  gpio write 15 1
 fi
